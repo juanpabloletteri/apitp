14da684d6dde1107540cc670eb5289563c038898
juanpabloletteri <pabloletteri@gmail.com>
2018-06-21 00:03:45 -0300

    db

--- db/tp.sql
+++ db/tp.sql
@@ -2,8 +2,8 @@
 -- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2018 a las 01:36:57
+-- Servidor: localhost
+-- Tiempo de generación: 21-06-2018 a las 05:03:15
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.4
 
@@ -33,6 +33,17 @@ CREATE TABLE `choferes` (
   `id_usuario` int(11) NOT NULL,
   `legajo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `choferes`
+--
+
+INSERT INTO `choferes` (`id_chofer`, `id_usuario`, `legajo`) VALUES
+(1, 19, 31122),
+(2, 20, 31122),
+(3, 21, 31122),
+(4, 22, 1233),
+(5, 23, 123);
 
 -- --------------------------------------------------------
 
@@ -71,7 +82,8 @@ INSERT INTO `clientes` (`id_cliente`, `id_usuario`, `domicilio`) VALUES
 (6, 15, '13132'),
 (7, 16, '13132'),
 (8, 17, '13132'),
-(9, 18, '13132');
+(9, 18, '13132'),
+(10, 24, '234234');
 
 -- --------------------------------------------------------
 
@@ -126,15 +138,21 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id_usuario`, `mail`, `password`, `nombre`, `apellido`, `dni`, `telefono`, `tipo`) VALUES
 (9, 'juanp@gmail.com', 'asd123', 'joselooooo', 'perez', 36451289, 142233654, 1),
-(10, 'mail', 'uberto29124119', 'jujan', 'lelrkelr', 29124119, 2147483647, 3),
-(11, 'mail', 'uberto29124119', 'jujan', 'lelrkelr', 29124119, 2147483647, 3),
-(12, 'mail', 'uberto29124119', 'jujan', 'lelrkelr', 29124119, 2147483647, 3),
-(13, 'mail', 'uberto29124119', 'jujan', 'lelrkelr', 29124119, 2147483647, 3),
-(14, 'mail', 'uberto29124119', 'jujan', 'lelrkelr', 29124119, 2147483647, 3),
-(15, 'mail', 'uberto16546461', '32132132', '132132', 16546461, 2147483647, 3),
-(16, 'mail', 'uberto16546461', '32132132', '132132', 16546461, 2147483647, 3),
-(17, 'mailssss', 'uberto16546461', '32132132', '132132', 16546461, 2147483647, 3),
-(18, 'mailssssqweqwe', 'uberto16546461', 'qweqwe', '132132', 16546461, 2147483647, 3);
+(10, 'mail', 'uberto29124119', 'Pablo', 'Frenkel', 29544119, 2147483647, 3),
+(11, 'mail', 'uberto29124119', 'Julian', 'Guibaudo', 29132219, 2147483647, 3),
+(12, 'mail', 'uberto29124119', 'Ariel', 'Lambezat', 33124119, 2147483647, 3),
+(13, 'mail', 'uberto29124119', 'Natalia', 'Labonia', 29124119, 2147483647, 3),
+(14, 'mail', 'uberto29124119', 'Giselle', 'Molina', 29124119, 2147483647, 3),
+(15, 'mail', 'uberto16546461', 'Estefania', 'Villalba', 16546461, 2147483647, 3),
+(16, 'mail', 'uberto16546461', 'Rogelio', 'Aguada', 16546461, 2147483647, 3),
+(17, 'mailssss', 'uberto16546461', 'Matias', 'Lenriques', 16546461, 2147483647, 3),
+(18, 'mailssssqweqwe', 'uberto16546461', 'Lorena', 'Encinas', 16546461, 2147483647, 3),
+(19, 'chofer@gmail.com', 'uberto12312312', 'Juan', 'Mazzedo', 29125567, 1233434343, 2),
+(20, 'chofer@gmail.com', 'uberto12312312', 'Jose', 'Fernandez', 36987632, 1233434343, 2),
+(21, 'chofer@gmail.com', 'uberto12312312', 'Enrique', 'Messi', 23456324, 1233434343, 2),
+(22, 'chof@gm.com', 'uberto1233123', 'Roberto', 'Machuca', 31459835, 312312312, 2),
+(23, 'jum@com.com', 'uberto1233321', 'Maria', 'Sanchez', 21777283, 321321, 2),
+(24, '21@asd.cpom', 'uberto123', 'Eugenio', 'Labonia', 27123332, 432423, 3);
 
 -- --------------------------------------------------------
 
@@ -158,8 +176,11 @@ CREATE TABLE `vehiculos` (
 --
 
 INSERT INTO `vehiculos` (`id_vehiculo`, `id_chofer`, `marca`, `modelo`, `anio`, `fumar`, `aire`, `baul`) VALUES
-(3, 0, 'chevrolet', 'corsa', 2015, 0, 0, 0),
-(4, 0, 'fiat', 'palio', 2017, 1, 1, 0);
+(3, 1, 'chevrolet', 'corsa', 2015, 0, 0, 0),
+(4, 2, 'fiat', 'palio', 2017, 1, 1, 0),
+(5, 3, 'toyota', 'etios', 2017, 1, 0, 1),
+(6, 4, 'chevrolet', 'corsa', 2015, 1, 1, 0),
+(7, 5, 'chevrolet', 'corsa', 2016, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -171,13 +192,16 @@ CREATE TABLE `viajes` (
   `id_viaje` int(11) NOT NULL,
   `id_encargado` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
-  `id_chofer` int(11) NOT NULL,
-  `direccion_inicio` varchar(50) NOT NULL,
-  `direccion_destino` varchar(50) NOT NULL,
+  `id_vehiculo` int(11) NOT NULL,
+  `latitud_inicio` int(30) NOT NULL,
+  `longitud_inicio` int(30) NOT NULL,
+  `latitud_destino` int(30) NOT NULL,
+  `longitud_destino` int(30) NOT NULL,
   `distancia` int(11) NOT NULL,
-  `estado` int(11) NOT NULL,
   `costo` int(11) NOT NULL,
-  `forma_pago` int(11) NOT NULL
+  `forma_pago` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -239,7 +263,7 @@ ALTER TABLE `viajes`
   ADD PRIMARY KEY (`id_viaje`),
   ADD KEY `id_encargado` (`id_encargado`),
   ADD KEY `id_cliente` (`id_cliente`),
-  ADD KEY `id_chofer` (`id_chofer`);
+  ADD KEY `id_chofer` (`id_vehiculo`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -249,13 +273,13 @@ ALTER TABLE `viajes`
 -- AUTO_INCREMENT de la tabla `choferes`
 --
 ALTER TABLE `choferes`
-  MODIFY `id_chofer` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_chofer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `encargados`
@@ -273,13 +297,13 @@ ALTER TABLE `puntajes`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `vehiculos`
 --
 ALTER TABLE `vehiculos`
-  MODIFY `id_vehiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_vehiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `viajes`
@@ -328,7 +352,7 @@ ALTER TABLE `puntajes`
 ALTER TABLE `viajes`
   ADD CONSTRAINT `viajes_ibfk_1` FOREIGN KEY (`id_encargado`) REFERENCES `encargados` (`id_encargado`),
   ADD CONSTRAINT `viajes_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
-  ADD CONSTRAINT `viajes_ibfk_3` FOREIGN KEY (`id_chofer`) REFERENCES `choferes` (`id_chofer`);
+  ADD CONSTRAINT `viajes_ibfk_3` FOREIGN KEY (`id_vehiculo`) REFERENCES `choferes` (`id_chofer`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
